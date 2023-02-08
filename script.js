52da7cf7f717f32810be901d04e84a8e692703ac
Pedro Gabriel <pedrogabriel.santos@hotmail.com>
2023-02-08 13:19:01 -0300

    Commit for my news updates

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-const teste = 'teste';
-
-console.log(teste)
+const inputs = document.querySelectorAll('.input');
+const handleFocus = (event) => {
+    console.log(event.target);
+}
+inputs.forEach((input)=>input.addEventListener('focus', handleFocus));
